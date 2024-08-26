4b67a4dc5a01100751d723ed5d6c61511d16412b
tomit4 <mosssap@gmail.com>
2024-08-25 21:48:52 -0700

    :construction: Started work on main auth route, notes on jwt logic

--- backend/src/pikoshi/routers/auth_context.py
+++ backend/src/pikoshi/routers/auth_context.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException, Response
+from typing import Annotated
+
+import jwt
+from fastapi import APIRouter, Cookie, HTTPException, Response
 from fastapi.responses import JSONResponse
 
 from ..middlewares.logger import TimedRoute
+from ..services.jwt_service import ALGORITHM, SECRET_KEY, JWTAuthService
+from ..utils.logger import logger
 
 router = APIRouter(prefix="/auth", tags=["auth"], route_class=TimedRoute)
 
@@ -20,6 +25,49 @@ router = APIRouter(prefix="/auth", tags=["auth"], route_class=TimedRoute)
 # Email is stored in redis cache, where jwt hash is key, email is value
 # Find user in db by email and set is_active status to false if no longer
 # authenticated (i.e. logged out)
+
+
 @router.post("/auth-context/")
-async def check_auth_context():
-    return JSONResponse(status_code=200, content={"message": "AOKay!"})
+async def check_auth_context(
+    access_token: Annotated[str | None, Cookie()] = None,
+    refresh_token: Annotated[str | None, Cookie()] = None,
+) -> Response:
+    try:
+        if not access_token and not refresh_token:
+            raise HTTPException(
+                status_code=401,
+                detail="No Authentication Tokens Submitted For Authentication.",
+            )
+        # TODO: Implement logic re: refreshing of
+        # access_token using refresh_token logic
+        if not JWTAuthService.is_jwt(access_token):
+            print("is google-oauth2 access token")
+            #  authenticate_google_oauth_access_token(access_token)
+        else:
+            decoded_access_token = jwt.decode(
+                str(access_token),
+                SECRET_KEY,
+                algorithms=[ALGORITHM],
+            )
+            # NOTE: Consider using UUID instead of encrypted email as
+            # redis value associated with jwt sub?
+
+            # TODO: Once decoded decrypt encrypted email
+            # str here, and check if is in redis
+            if not decoded_access_token:
+                raise HTTPException(
+                    status_code=401, detail="Access Token Corrupted Or No Longer Valid."
+                )
+
+            print("decoded_access_token :=>", decoded_access_token.get("sub"))
+
+            #  authenticate_jwt_access_token(access_token)
+
+        return JSONResponse(status_code=200, content={"message": "AOKay!"})
+    except HTTPException as http_e:
+        raise HTTPException(status_code=http_e.status_code, detail=http_e.detail)
+    except jwt.exceptions.PyJWTError as jwt_e:
+        raise HTTPException(status_code=401, detail=jwt_e)
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {str(e)}")
+        raise HTTPException(status_code=500, detail="Internal Server Error.")
--- backend/src/pikoshi/services/jwt_service.py
+++ backend/src/pikoshi/services/jwt_service.py
@@ -12,14 +12,22 @@ ALGORITHM = str(os.environ.get("ALGORITHM"))
 
 class JWTAuthService:
     @staticmethod
+    #  def get_user_tokens(encrypted_email_str):
     def get_user_tokens():
-        access_token_expires = datetime.now(timezone.utc) + timedelta(hours=1)
-        refresh_token_expires = datetime.now(timezone.utc) + timedelta(hours=24)
+        # NOTE: Change each values to test invalidation of token logic
+        #  access_token_expires = datetime.now(timezone.utc) + timedelta(hours=1) # default
+        access_token_expires = datetime.now(timezone.utc) + timedelta(minutes=1)
+        refresh_token_expires = datetime.now(timezone.utc) + timedelta(
+            hours=24
+        )  # default
 
         access_token = jwt.encode(
             {
                 "exp": access_token_expires,
                 "iat": datetime.now(timezone.utc),
+                # TODO: Put encrypted email string here OR UUID, think on it...
+                # so that we can set is_active flag in DB to true/false
+                # and also against redis cache on reaffirming authentication (i.e. check_auth_context route)
                 "sub": "pikoshi access token",
             },
             SECRET_KEY,
@@ -36,3 +44,8 @@ class JWTAuthService:
         )
 
         return {"access_token": access_token, "refresh_token": refresh_token}
+
+    @staticmethod
+    def is_jwt(token):
+        """Check if a token is a JWT based on its structure."""
+        return token.count(".") == 2
