73e92082bfa465a947c4caaedca34498bde4ca4c
tomit4 <mosssap@gmail.com>
2024-08-29 17:37:29 -0700

    :recycle: Refactored Google OAuth Route/Service

--- backend/src/pikoshi/routers/google_auth.py
+++ backend/src/pikoshi/routers/google_auth.py
@@ -1,22 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException, Response
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
-from ..config.redis_config import redis_instance as redis
 from ..dependencies import get_db
 from ..middlewares.logger import TimedRoute
 from ..schemas.auth import AuthCodeRequest
+from ..services.exception_handler_service import ExceptionService
 from ..services.google_oauth_service import GoogleOAuthService
-from ..services.security_service import generate_salt, hash_value, verify_value
-from ..services.user_service import (
-    create_user,
-    generate_user_profile,
-    get_user_by_email,
-    set_user_as_active,
-)
-from ..utils.auth_cookies import set_auth_cookies
-from ..utils.logger import logger
 
 router = APIRouter(prefix="/auth", tags=["auth"], route_class=TimedRoute)
 
@@ -28,45 +17,23 @@ async def signup_with_google(
     try:
         auth_code = request.code
         user_tokens = await GoogleOAuthService.get_user_tokens(auth_code)
-        if not user_tokens:
-            raise HTTPException(
-                status_code=401, detail="Google Auth Code Not Authorized."
-            )
-
-        access_token = user_tokens.get("access_token")
-        refresh_token = user_tokens.get("refresh_token")
+        access_token = str(user_tokens.get("access_token"))
+        refresh_token = str(user_tokens.get("refresh_token"))
 
         user_info = await GoogleOAuthService.get_user_info(access_token)
-        if not user_info:
-            raise HTTPException(
-                status_code=401, detail="Google Access Token Not Authorized."
-            )
+        await GoogleOAuthService.signup_user_with_google(user_info, access_token, db)
 
-        # NOTE: Generates hash based off of user_info.id instead of password
-        user_id = user_info.get("id")
-        salt = generate_salt()
-        user_name = user_info.get("name")
-        user_email = user_info.get("email")
-        user_password = hash_value(user_id, salt)
-        new_user = generate_user_profile(user_name, user_password, user_email, salt)
-        new_user = create_user(db, new_user)
-        if not new_user:
-            raise HTTPException(
-                status_code=409, detail="Email has already been registered."
-            )
-        await redis.set(f"auth_session_{access_token}", user_id, ex=3600)
-        set_user_as_active(db, new_user)
-
-        response = jsonable_encoder(
-            {"message": "User Authenticated, setting credentials."}
+        response = GoogleOAuthService.set_authenticated_response(
+            access_token, refresh_token
         )
-        response = JSONResponse(status_code=200, content=response)
-        response = set_auth_cookies(response, access_token, refresh_token)
 
         return response
+    except HTTPException as http_e:
+        return ExceptionService.handle_http_exception(http_e)
+    except ValueError as ve:
+        return ExceptionService.handle_value_exception(ve)
     except Exception as e:
-        logger.error(f"An unexpected error occurred: {str(e)}")
-        raise HTTPException(status_code=500, detail="Internal Server Error.")
+        return ExceptionService.handle_generic_exception(e)
 
 
 @router.post("/google-login/")
@@ -75,53 +42,25 @@ async def login_with_google(
 ) -> Response:
     try:
         auth_code = request.code
-
         user_tokens = await GoogleOAuthService.get_user_tokens(auth_code)
-        if not user_tokens:
-            raise HTTPException(
-                status_code=401, detail="Google Auth Code Not Authorized."
-            )
-
-        access_token = user_tokens.get("access_token")
-        refresh_token = user_tokens.get("refresh_token")
+        access_token = str(user_tokens.get("access_token"))
+        refresh_token = str(user_tokens.get("refresh_token"))
 
         user_info = await GoogleOAuthService.get_user_info(access_token)
-        if not user_info:
-            raise HTTPException(
-                status_code=401, detail="Google Access Token Not Authorized."
-            )
+        user_from_db = GoogleOAuthService.get_user_by_email_from_db(user_info, db)
 
-        user_email = user_info.get("email")
-        user_from_db = get_user_by_email(db, user_email)
-        if not user_from_db:
-            raise HTTPException(status_code=400, detail="No User By That Email Found")
-
-        # Generates a hash from the user_id (from the google oauth code)
-        # and the user_salt (from the DB)
-        # and then compares the hash generated to the hash in the DB (user_password)
-        user_id = user_info.get("id")
-        user_password = str(user_from_db.password)
-        user_salt = str(user_from_db.salt)
-        user_is_verified = verify_value(user_id, user_password, user_salt)
-
-        if not user_is_verified:
-            raise HTTPException(status_code=401, detail="Hashes in DB do not match")
-
-        # TODO:
-        # [x] Set user.is_active to true in DB,
-        # [ ] update user.last_login to now
-        user_id = user_from_db.id
-        if isinstance(user_id, int):
-            await redis.set(f"auth_session_{access_token}", user_id, ex=3600)
-        set_user_as_active(db, user_from_db)
-
-        response = jsonable_encoder(
-            {"message": "User Authenticated, setting credentials."}
+        await GoogleOAuthService.authenticate_user_with_google(
+            user_info, user_from_db, access_token, db
         )
-        response = JSONResponse(content=response)
-        response = set_auth_cookies(response, access_token, refresh_token)
+
+        response = GoogleOAuthService.set_authenticated_response(
+            access_token, refresh_token
+        )
 
         return response
+    except HTTPException as http_e:
+        return ExceptionService.handle_http_exception(http_e)
+    except ValueError as ve:
+        return ExceptionService.handle_value_exception(ve)
     except Exception as e:
-        logger.error(f"An unexpected error occurred: {str(e)}")
-        raise HTTPException(status_code=500, detail="Internal Server Error.")
+        return ExceptionService.handle_generic_exception(e)
--- backend/src/pikoshi/services/google_oauth_service.py
+++ backend/src/pikoshi/services/google_oauth_service.py
@@ -3,12 +3,22 @@ from typing import Dict
 
 import httpx
 from dotenv import load_dotenv
-from fastapi import Depends
+from fastapi import Depends, HTTPException, Response
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
+from ..config.redis_config import redis_instance as redis
 from ..dependencies import get_db
 from ..schemas.user import User
-from ..services.user_service import get_user_by_email
+from ..services.security_service import generate_salt, hash_value, verify_value
+from ..services.user_service import (
+    create_user,
+    generate_user_profile,
+    get_user_by_email,
+    set_user_as_active,
+)
+from ..utils.auth_cookies import set_auth_cookies
 
 load_dotenv()
 
@@ -60,3 +70,84 @@ class GoogleOAuthService:
         if not user:
             raise ValueError("User not found in DB")
         return user
+
+    @staticmethod
+    # NOTE: User schema needs to be adjusted to return User type in this case...
+    def get_user_by_email_from_db(user_info, db: Session = Depends(get_db)):
+        user_email = str(user_info.get("email"))
+        user_from_db = get_user_by_email(db, user_email)
+        if not user_from_db:
+            raise HTTPException(status_code=400, detail="No User By That Email Found")
+        return user_from_db
+
+    @staticmethod
+    async def signup_user_with_google(
+        user_info, access_token: str, db: Session = Depends(get_db)
+    ) -> None:
+        """
+        - Creates a User instance in DB using fields grabbed from Google OAuth2 Services.
+        - Generates Unique Salt and stores it in DB.
+        - Utilizes the user_id instead of a user inputted password for
+          password field since we don't have access to user's actual password
+          using this sign up method (also hashes the user_id as if it were a password).
+        - Creates a user session in redis, using the access_token as the key,
+          and the new_user.id as the value. Lasts as long as the default access_token expiry.
+        - Toggles the user's is_active field in the DB to True.
+        """
+        user_id = str(user_info.get("id"))
+        salt = generate_salt()
+        user_name = user_info.get("name")
+        user_email = user_info.get("email")
+        user_password = hash_value(user_id, salt)
+        new_user = generate_user_profile(user_name, user_password, user_email, salt)
+        new_user = create_user(db, new_user)
+        if not new_user:
+            raise HTTPException(
+                status_code=409, detail="Email has already been registered."
+            )
+        await redis.set(
+            f"auth_session_{access_token}", new_user.id, ex=3600  # type:ignore
+        )
+        set_user_as_active(db, new_user)
+
+    @staticmethod
+    async def authenticate_user_with_google(
+        user_info, user_from_db, access_token: str, db: Session = Depends(get_db)
+    ) -> None:
+        """
+        - Grabs the User's Google Id from Google OAuth2 Services.
+        - Grabs the User's hashed/salted/peppered password from the DB.
+        - Grabs the User's salt from the DB.
+        - Verifies that the hashed/salted/peppered user_id matches the
+          password retreived from the DB.
+        - Grabs the User's Pikoshi DB id (different from Google ID)
+        - Sets in the redis cache the session via the access_token as the key,
+          and the user id from the DB as the value. Lasts as long as the default
+          access_token expiry.
+        - Toggles the user's is_active field in the DB to True.
+        """
+        # Generates a hash from the user_id (from the google oauth code)
+        # and the user_salt (from the DB)
+        # and then compares the hash generated to the hash in the DB (user_password)
+        user_id = str(user_info.get("id"))
+        user_password = str(user_from_db.password)
+        user_salt = str(user_from_db.salt)
+        user_is_verified = verify_value(user_id, user_password, user_salt)
+        if not user_is_verified:
+            raise HTTPException(status_code=401, detail="Hashes in DB do not match")
+
+        # TODO:
+        # [x] Set user.is_active to true in DB,
+        # [ ] update user.last_login to now
+        user_id = user_from_db.id
+        await redis.set(f"auth_session_{access_token}", user_id, ex=3600)  # type:ignore
+        set_user_as_active(db, user_from_db)
+
+    @staticmethod
+    def set_authenticated_response(access_token, refresh_token) -> Response:
+        response = jsonable_encoder(
+            {"message": "User Authenticated, setting credentials."}
+        )
+        response = JSONResponse(status_code=200, content=response)
+        response = set_auth_cookies(response, access_token, refresh_token)
+        return response
