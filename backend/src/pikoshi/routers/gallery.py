49693464f6a7d1c9863959024817a01a8999ae0a
tomit4 <mosssap@gmail.com>
2024-09-29 23:54:53 -0700

    :bug: Fixed bug with initial image loading

--- backend/src/pikoshi/routers/gallery.py
+++ backend/src/pikoshi/routers/gallery.py
@@ -1,7 +1,14 @@
 from typing import Annotated
 
-from fastapi import (APIRouter, Body, Cookie, Depends, HTTPException, Response,
-                     UploadFile)
+from fastapi import (
+    APIRouter,
+    Body,
+    Cookie,
+    Depends,
+    HTTPException,
+    Response,
+    UploadFile,
+)
 from fastapi.responses import JSONResponse, StreamingResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
